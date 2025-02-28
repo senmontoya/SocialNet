0d0a0a6b2b5d2fd2101b8b9d26f7592a2c506f6c
DamianDev-Heaven <harryadonay62@gmail.com>
2025-02-28 13:19:12 -0600

    changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rjinofvjhblfavdhiwjg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqaW5vZnZqaGJsZmF2ZGhpd2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxMDI4MzIsImV4cCI6MjA1NTY3ODgzMn0.7iWsOG87Oe1hg3UvPr1ezQLhIlPmOswNTMg39lE27y0';
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: window.sessionStorage,
